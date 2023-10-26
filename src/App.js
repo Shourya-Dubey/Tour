8ab6a13c547b3791a4bcde4b3d43cf633afa30ae
Shouryadubey <Shourya.dubey1999@gmial.com>
2023-10-26 10:39:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from './Data';
+import { useState } from 'react';
+import Tours from './component/Tours';
 
 function App() {
+
+  const [tours, setTours] = useState(Data);
+
+  function removeTour(id){
+    const newTour = tours.filter( tour=> tour.id !== id);
+    setTours(newTour);
+  }
+
+  if(tours.length === 0){
+    return (<div className='refresh'>
+      <h2>No Tours Left</h2>
+      <button className='btn-white' onClick={() => setTours(Data)}>
+        Refresh
+      </button>
+    </div>)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Tours tours={tours} removeTours={removeTour}></Tours>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,54 @@
+const Data =[
+    {
+        id:1,
+        name: "Agra",
+        info: `Agra is a city in nothern India, known for the iconic Taj mahal a stunning white marble mauoleum built by mugharl emperor shah jahan in memory of his beloves wife. Agra is also home to several other `,
+        image: "https://assets.editorial.aetnd.com/uploads/2011/06/taj-mahal-gettyimages-463924915.jpg",
+        price: "35,758",
+    },    
+    {
+        id:2,
+        name: "jaipur",
+        info: `Jaipur derives its name from its founder, Sawai Jai Singh (1693-1744), a notable warrior and accomplished astronomer.[17][18] In Sanskrit, variations of the word "pur" or "pura" are commonly used to refer to a city or town. Therefore, "Jaipur" essentially means "The City of Jai" or "Jai's City," paying homage to Maharaja Jai Singh II, who established the city. `,
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/41/East_facade_Hawa_Mahal_Jaipur_from_ground_level_%28July_2022%29_-_img_01.jpg",
+        price: "82,196",
+    },    
+    {
+        id:3,
+        name: "goa",
+        info: `Goa (Konkani is a state on the southwestern coast of India within the Konkan region, geographically separated from the Deccan highlands by the Western Ghats.[6][7] It is bound by the Indian states of Maharashtra to the north, and Karnataka `,
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/f0/goa.jpg?w=700&h=500&s=1",
+        price: "40,500",
+    },    
+    {
+        id:4,
+        name: "varanasi",
+        info: `Varanasi is one of the world's oldest continually inhabited cities.[21] Kashi, its ancient name, was associated with a kingdom of the same name of 2,500 years ago. The Lion capital of Ashoka at nearby Sarnath has been interpreted to be a commemoration of th`,
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/be/17/41/caption.jpg?w=700&h=500&s=1",
+        price: "35,432",
+    },    
+    {
+        id:5,
+        name: "jaisalmer",
+        info: `Jaisalmer was founded by Jaisal Singh, popularly known as Rawal Jaisal,[8] in 1156 AD.[9] It is named after its founder, with "Jaisal" representing the king's name and "Mer" signifying a fort. So, it means "The Fort of Jaisal", emphasizing the city's historic `,
+        image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/09/Thar-Desert.jpg",
+        price: "87,100",
+    },    
+    {
+        id:6,
+        name: "kochi",
+        info: `Kochi is known as the financial,[23][24] commercial[25][26] and industrial[27][28] capital of Kerala. It has the highest GDP as well as the highest GDP per capita in the state.[29][30] The city is home to the Southern Naval Command of the Indian Navy and is the state headquarters[31][32] of the Indian Coast Guard. Commercial maritime facilities of the `,
+        image: "https://lp-cms-production.imgix.net/2020-11/f8ccb1f5c85e6cba4b70b879ac5f4e13-chinese-fishing-nets.jpg",
+        price: "55,000",
+    },    
+    {
+        id:7,
+        name: "Darjeeling",
+        info: `Darjeeling's population today is constituted largely of the descendants of the indigenous and immigrant labourers that were employed in the original development of the town. Although their common language, the Nepali language, has been given official `,
+        image: "https://hikerwolf.com/wp-content/uploads/2020/04/Darjeeling-toy-train-route.jpg",
+        price: "72,521",
+    },
+    
+]
+
+export default Data;
